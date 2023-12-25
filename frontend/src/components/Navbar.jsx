34ef1fe7fc4da9af06a13b2ebbeeccc316d46927
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2023-12-25 20:29:36 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ export default function Navbar() {
       <nav className='flex justify-between items-center m-0 p-8 bg-slate-900 text-slate-200 w-full h-[100px] sticky top-0 border-b border-slate-800'>
         <div className='text-3xl font-black'>CineMagic</div>
         <div className='flex justify-around items-center'>
-          <div className='text-[20px] p-2 m-5 font-semibold hover:font-bold'>About</div>
-          <div className='text-[20px] p-2 m-5 font-semibold hover:font-bold'>Contact</div>
+          <div className='text-[20px] p-2 m-5 font-semibold hover:font-bold invisible'>About</div>
+          <div className='text-[20px] p-2 m-5 font-semibold hover:font-bold invisible'>Contact</div>
           {login ? (
             <Link to={`/myprofile/${email}`}>
               <div className='text-[20px] p-3 m-5 font-semibold hover:font-bold bg-slate-200 text-slate-900 rounded-xl'>

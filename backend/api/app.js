b811f4bb3b4f1f66be875c7791467094beae1fb1
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2024-01-12 10:42:15 +0530

    Update app.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -186,7 +186,7 @@ app.get('/myprofile/:id', async (req, res) => {
   }
 });
 
-
-app.listen(8080,()=>{
-    console.log('server running on port 8080')
+const PORT=process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log('server running on port',PORT)
 })

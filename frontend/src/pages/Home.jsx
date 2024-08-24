287abdb0624488e75e10024ea116b0e98bc73d67
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2024-08-24 14:55:14 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,12 @@ import Navbar from '../components/Navbar'
 import Genre from '../components/Genre'
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
 
 
 export default function Home() {
+  const { login,email, logoutHandler } = useAuth();
+const backend="https://demo-omega-ochre.vercel.app"
   const[movies,setMovies]=useState([])
   useEffect(() => {
     axios.get('https://demo-omega-ochre.vercel.app/')
@@ -17,28 +20,77 @@ export default function Home() {
         console.log('error getting the data');
       });
   }, []);
+
+  const renderStars = (rating) => {
+    const totalStars = 5;
+    return Array.from({ length: totalStars }, (v, i) => {
+      if (i < Math.floor(rating)) {
+        return <i key={i} className="fa-solid fa-star"></i>;
+      } else if (i < rating) {
+        return <i key={i} className="fa-solid fa-star-half-stroke"></i>;
+      } else {
+        return <i key={i} className="fa-regular fa-star"></i>;
+      }
+    });
+  };
+
+  // Chunk movies into groups of 5
+  const movieRows = [];
+  for (let i = 0; i < movies.length; i += 5) {
+    movieRows.push(movies.slice(i, i + 5));
+  }
   
+
   return (
-    <div className='m-0 min-h-screen w-full bg-slate-900'>
-         <Navbar/>
-        <div className='p-5 text-slate-200 '>
-          <h1 className='text-4xl m-5'>Latest Movies In Theatre Now</h1>
-           <div className='flex flex-wrap justify-left items-center h-auto m-10 mt-18 p-2'>
-                {
-                  movies.map((movie=>{
-                    return(
-                    <div className='m-5' key={movie._id}>
-                    <Link to={`/details/${movie._id}`}> 
-                    <img src={movie.link} alt={movie.title} className='h-[300px]' />
+      <div className="screen  w-screen h-screen flex flex-row poppins-regular">
+            <Navbar/>
+          <div class="rightsidebox basis-4/5 p-10 text-white bg-[#100f2b]">
+             
+          <div class="navbar flex justify-end items-center w-[100%] h-[20px] text-xl pr-10 !poppins-extralight">
+                <Link  to="/" class="flex">
+                    <i class="fa-solid fa-house"></i>
+                    <p class="pr-20 pl-2">Home</p>
+                </Link>
+                <div class="flex">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p class="pr-20 pl-2">Bengaluru</p>
+                </div>
+
+              
+                <div class="rounded-[30px] bg-[#f09631] p-3 border-slate-300 text-white  ">
+                {login ? (
+                      <Link to={`/myprofile/${email}`}>
+                        <div >My Profile</div>
+                      </Link>
+                    ) : (
+                      <Link to='/login'>
+                        <div>LogIn</div>
+                      </Link>
+                    )}
+                </div>
+            </div>
+
+
+            <div className="bigmoviebox  w-[100%] h-[97%] mt-7 flex flex-col ">
+            {movieRows.map((row, rowIndex) => (
+            <div key={rowIndex} className="movierow w-full basis-1/2 flex rounded pt-3">
+              {row.map((movie) => (
+                <div className="moviecol basis-1/5 flex flex-col py-2 px-6" key={movie._id}>
+                  <div className="poster basis-4/5 bg-blue-100 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+                    <Link to={`/details/${movie._id}`}>
+                      <img src={movie.link} alt={movie.title} className='h-[250px] w-[200px]'/>
                     </Link>
-                    <center> <p className='text-xl mt-2'>Ratings: {movie.rating}</p></center>
-                    </div>
-                    )
-                  }))
-                }
-           </div>
-        </div>
-       <Genre/>
-    </div>
+                  </div>
+                  <center className="rating basis-1/5 mt-2 text-[#f09631]">
+                    {renderStars(movie.rating)}
+                  </center>
+                </div>
+              ))}
+            </div>
+          ))}
+            </div>
+          
+          </div>   
+      </div>
   )
 }
